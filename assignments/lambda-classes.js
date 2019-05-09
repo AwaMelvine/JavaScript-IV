09d16f3d52ed6cef9f500773491969d14657c54f
Melvin Khan-Awa <melvinkhan-awa@Melvins-MacBook-Pro.local>
2019-05-09 13:37:03 +0100

    Create the Instructor class and define its methods

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,5 +1,6 @@
 // CODE here for your Lambda Classes
 
+// Person Class
 class Person {
   constructor(personData) {
     this.name = personData.name;
@@ -10,5 +11,23 @@ class Person {
 
   speak() {
     return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
+}
+
+// Instructor Class
+class Instructor extends Person {
+  constructor(object) {
+    super(object);
+    this.specialty = object.specialty;
+    this.favLanguage = object.favLanguage;
+    this.catchPhrase = object.catchPhrase;
+  }
+
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}`);
+  }
+
+  grade(student, subject) {
+    console.log(`${student.name} receives a perfect score on ${subject}`);
   }
 }
