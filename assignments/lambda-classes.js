0bdb0b7f104c37eae088b13065938e455237e04c
Melvin Khan-Awa <melvinkhan-awa@Melvins-MacBook-Pro.local>
2019-05-09 13:28:05 +0100

    Define a Person class with construtor and method

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,14 @@
 // CODE here for your Lambda Classes
+
+class Person {
+  constructor(personData) {
+    this.name = personData.name;
+    this.location = personData.location;
+    this.age = personData.age;
+    this.gender = personData.gender;
+  }
+
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
+}
